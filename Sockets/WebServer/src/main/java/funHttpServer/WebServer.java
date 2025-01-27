c252b3a845053e4319abecfd2ba5679e1d855b0f
jmelcho5 <jmelcho5@asu.edu>
2025-01-27 15:54:36 -0700

    Fix additional errors in multiply case 8

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -201,37 +201,28 @@ class WebServer {
           // extract path parameters
           query_pairs = splitQuery(request.replace("multiply?", ""));
 
-//          try {
-//            // extract required fields from parameters
-//            Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-//            Integer num2 = Integer.parseInt(query_pairs.get("num2"));
-//
-//            // do math
-//            Integer result = num1 * num2;
-//
-//            // Generate response
-//            builder.append("HTTP/1.1 200 OK\n");
-//            builder.append("Content-Type: text/html; charset=utf-8\n");
-//            builder.append("\n");
-//            builder.append("Result is: " + result);
-//          }
+
+          // extract required fields from parameters
+          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
+          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+
+          // do math
+          Integer result = num1 * num2;
+
+          // Generate response
+          builder.append("HTTP/1.1 200 OK\n");
+          builder.append("Content-Type: text/html; charset=utf-8\n");
+          builder.append("\n");
+          builder.append("Result is: " + result);
 
           // TODO: Include error handling here with a correct error code and
           // a response that makes sense
-//          Integer number1 = null;
-//          Integer number2 = null;
-
-          Integer number1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer number2 = Integer.parseInt(query_pairs.get("num2"));
+          Integer number1;
+          Integer number2;
 
           try {
-            Integer multiply = number1 * number2;
-
-            builder.append("HTTP/1.1 200 OK\n");
-            builder.append("Content-Type: text/html; charset=utf-8\n");
-            builder.append("\n");
-            builder.append("Result is: " + multiply);
-
+            number1 = num1;
+            number2 = number2;
           } catch (NullPointerException nullPointerException) {
             if (number1 == null || number2 == null || (number1 == null && number2 == null)) {
               builder.append("HTTP/1.1 400 Bad Request\n");
