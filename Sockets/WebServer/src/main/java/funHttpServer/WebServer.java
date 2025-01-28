e8f4bcd3432d68fd8fc298ea0265197066ce8553
jmelcho5 <jmelcho5@asu.edu>
2025-01-28 14:19:59 -0700

    GitHub implementation 20

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -317,7 +317,7 @@ class WebServer {
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
             builder.append("Please enter query, e.g. query=users/OWNERNAME/repos\n");
-          } else if (!queryParam[0].equals("user") || !queryParam[2].equals("repos")) {
+          } else if (!queryParam[0].equals("users") || !queryParam[2].equals("repos")) {
             builder.append("HTTP/1.1 400 Bad Request\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
