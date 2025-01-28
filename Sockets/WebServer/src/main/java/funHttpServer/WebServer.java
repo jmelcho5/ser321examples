3ac353832eef9f198e8c9bed7851a7f1a190fe4f
jmelcho5 <jmelcho5@asu.edu>
2025-01-28 16:59:31 -0700

    Request1 current grade 7

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -397,20 +397,20 @@ class WebServer {
             builder.append("\n");
             builder.append("Please enter the quiz parameter, e.g. quiz=85\n");
           } else {
-
             for (String key : query_pairs.keySet()) {
               if (error == false) {
                 if (!key.equals("assign") || !key.equals("quiz") || !key.equals("exam")) {
-                  builder.append("HTTP/1.1 406 Not Acceptable\n");
-                  builder.append("Content-Type: text/html; charset=utf-8\n");
-                  builder.append("\n");
-                  builder.append("Please enter assignment, quiz, and exam scores only.\n");
                   error = true;
                 }
               }
             }
 
-            if (error == false) {
+            if (error == true) {
+              builder.append("HTTP/1.1 406 Not Acceptable\n");
+              builder.append("Content-Type: text/html; charset=utf-8\n");
+              builder.append("\n");
+              builder.append("Please enter assignment, quiz, and exam scores only.\n");
+            } else {
               try {
                 assignment = Double.parseDouble(query_pairs.get("assign"));
                 quiz = Double.parseDouble(query_pairs.get("quiz"));
